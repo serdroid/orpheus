6e55c37d9885f64fb892f6bfe0e721ce3ef7f39f
serdar severcan <serdar.severcan@cs.com.tr>
2019-02-05 13:51:05 +0300

    add query parameters to AuthorizationEndpoint.

--- src/main/java/info/serdroid/orpheus/rest/AuthorizationEndpoint.java
+++ src/main/java/info/serdroid/orpheus/rest/AuthorizationEndpoint.java
@@ -4,9 +4,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
@@ -25,10 +29,27 @@ public class AuthorizationEndpoint {
 
     @GET
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-    public Response authorize() {
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)    
+    public Response authorize(@QueryParam("response_type") String response_type, 
+	    	@QueryParam("client_id") String client_id,
+			@QueryParam("redirect_uri") String redirect_uri,
+			@QueryParam("scope") String scope,
+			@QueryParam("state") String state,
+			@QueryParam("code_challenge") String code_challenge,
+			@QueryParam("code_challenge_method") String code_challenge_method ) {
+    	AuthorizationRequest authorizationRequest = new AuthorizationRequest.Builder()
+    			.setResponseType(response_type)
+    			.setClientId(client_id)
+    			.setRedirectURI(redirect_uri)
+    			.setScope(scope)
+    			.setState(state)
+    			.setCodeChallenge(code_challenge)
+    			.setCodeChallengeMethod(code_challenge_method)
+    			.build();
     	URI location;
 		try {
-			location = new URI("../login.html?response_type=code&state=before_authenticate");
+			System.out.println("AuthorizationEndpoint : redirecting to login.html");
+			location = new URI("../login.html" + authorizationRequest.toURIString());
 			ResponseBuilder respBuilder = Response.status(Status.FOUND).location(location);
 	    	return respBuilder.build();
 		} catch (URISyntaxException e) {
@@ -38,5 +59,4 @@ public class AuthorizationEndpoint {
 		return Response.serverError().build();
     }
 
-	
 }
