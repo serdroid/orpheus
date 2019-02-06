79967aecc5d4f3b9c24cd29f06c5674bcd651674
serdar severcan <serdar.severcan@cs.com.tr>
2019-02-06 10:06:18 +0300

    introduce AuthorizationService to save authorization request and do validations.

--- src/main/java/info/serdroid/orpheus/AuthorizationService.java
+++ src/main/java/info/serdroid/orpheus/AuthorizationService.java
@@ -1,5 +1,31 @@
 package info.serdroid.orpheus;
 
-public class AuthorizationService {
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AuthorizationService {
+	Map<String, AuthorizationRequest> authRequests;
+	
+	public AuthorizationService() {
+		authRequests = new HashMap<>();
+	}
+
+	public void addAuthorizationRequest(String code, AuthorizationRequest authorizationRequest) {
+		authRequests.put(code, authorizationRequest);
+	}
+
+	public AuthorizationRequest getAuthorizationRequest(String code) {
+		return authRequests.get(code);
+	}
+
+	public AuthorizationRequest getAndRemoveAuthorizationRequest(String code) {
+		return authRequests.remove(code);
+	}
+	
+	public void validateTokenRequest(TokenRequest tokenRequest) {
+		
+	}
 }
